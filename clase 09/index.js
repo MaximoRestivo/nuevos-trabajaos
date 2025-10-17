25acdb09ccf0a7ee22ac2b29fc49d8fdb237ff49
MaximoRestivo <rmaximo@udesa.edu.ar>
2025-10-17 10:43:42 -0300

    funciones

---
+++ clase 09/index.js
@@ -0,0 +1,58 @@
+function rectangle (h,w){
+let area = h*w;
+return area;
+}
+let rectangulo = rectangle(10, 10)
+console.log(rectangulo)
+
+
+function triangle (p,j) {
+let area = (p*j)/2;
+return area;
+}
+let triangle(10, 10)
+console.log(triangulo)
+
+
+function dolarhoy(pesos){
+let preciodls = pesos*1455
+return preciodls
+}
+let dolarhoy(100000)
+console.log(dolar)
+
+
+function preciofinal(precio){
+let iva = precio + precio * 0.21
+return iva
+}
+let preciofinal (100000)
+console.log(precioo)
+
+
+function mitad(num){
+let mitad = num/2
+return mitad;
+}
+let mitad (100000)
+console.log(half)
+
+
+function suma(num1, num2){
+return num1 + num2;
+}
+let suma (10,10)
+console.log(sumation)
+
+
+function resta(num1,num2){
+    return num1 - num2;
+}
+let resta (10, 10)
+console.log(substract)
+
+function multiplicacion(num1, num2){
+    return(num1 * num2)
+}
+let multiplicacion(10, 10)
+console.log(multiplication)
